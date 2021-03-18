dab3cce2aa128e15a14ae9d7adac3316dc6ba60b
gEE <mail@mickeygee.com>
2021-03-18 14:26:35 +0100

    initial upload

---
+++ google-form-discord-webhook.js
@@ -0,0 +1,60 @@
+// Google Script for Google Forms, to pass content to a webhook
+// Remember to add a trigger aswell
+
+var POST_URL = "https://discord.com/api/webhooks/820101018901151744/hj2qsvVftJBEcMhArII_FP5IvnZTgPl6avgji18IHU1UU632F_IuFZlJSNlQZQ3yisOS";
+
+function onSubmit(e) {
+    var form = FormApp.getActiveForm();
+    var allResponses = form.getResponses();
+    var latestResponse = allResponses[allResponses.length - 1];
+    var response = latestResponse.getItemResponses();
+    var items = [];
+
+    for (var i = 0; i < response.length; i++) {
+        var question = response[i].getItem().getTitle();
+        var answer = response[i].getResponse();
+        try {
+            var parts = answer.match(/[\s\S]{1,1024}/g) || [];
+        } catch (e) {
+            var parts = answer;
+        }
+
+        if (answer == "") {
+            continue;
+        }
+        for (var j = 0; j < parts.length; j++) {
+            if (j == 0) {
+                items.push({
+                    "name": question,
+                    "value": parts[j],
+                    "inline": false
+                });
+            } else {
+                items.push({
+                    "name": question.concat(" (cont.)"),
+                    "value": parts[j],
+                    "inline": false
+                });
+            }
+        }
+    }
+
+    var options = {
+        "method": "post",
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "payload": JSON.stringify({
+            "embeds": [{
+                "title": "Here goes title",
+              "color": FF69B4, // This is optional, you can look for decimal colour codes at https://www.webtoolkitonline.com/hexadecimal-decimal-color-converter.html
+                "fields": items,
+                "footer": {
+                    "text": "---"
+                }
+            }]
+        })
+    };
+
+    UrlFetchApp.fetch(POST_URL, options);
+};
